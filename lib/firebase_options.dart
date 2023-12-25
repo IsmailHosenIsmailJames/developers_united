a4b79187c9cf6cd5a821d04bbf3295362177ef2e
MD Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2023-12-25 12:54:49 +0600

    App icon, splash creen and name changed

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -51,13 +51,13 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions web = FirebaseOptions(
     apiKey: 'AIzaSyC2Iat7HTtnKDAivp8uZ1xLKyuY0BXzMf8',
-    appId: '1:95856586450:web:d33f01d494694affd7070b',
+    appId: '1:95856586450:web:5b59214149d289add7070b',
     messagingSenderId: '95856586450',
     projectId: 'developersunited',
     authDomain: 'developersunited.firebaseapp.com',
     databaseURL: 'https://developersunited-default-rtdb.asia-southeast1.firebasedatabase.app',
     storageBucket: 'developersunited.appspot.com',
-    measurementId: 'G-B02Q04L8E6',
+    measurementId: 'G-54Y84C237L',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
