9897d70ec7b22dacb392b9e092da989dfa39cca9
MD Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-02-08 07:13:04 +0600

    upgraded pakage

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,7 +8,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
   firebase_storage
-  flutter_secure_storage_windows
   irondash_engine_context
   super_native_extensions
   url_launcher_windows
